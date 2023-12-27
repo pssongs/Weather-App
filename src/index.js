b20fc5ac90e40b281908f6df2ebb386abdb1d206
Paul Song <jaebinpsong@gmail.com>
2023-12-27 14:47:28 -0800

    Able to change city weather info from search bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const city = document.querySelectorAll('city')
+const city = document.getElementById('city')
 const condition = document.getElementById('condition')
 const temp = document.getElementById('temp')
 const high = document.getElementById('high')
 const low = document.getElementById('low')
+const search = document.querySelector('.search')
 
-
-async function getWeather(location) {
+async function getCurrent(location) {
   location = location.replace(" ","-")
   const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=03a0a509d6ee48609eb80436232612&q=${location}`)
   const info = await response.json()
@@ -13,14 +13,46 @@ async function getWeather(location) {
   return info
 }
 
+async function getFuture(location){
+  location = location.replace(' ','-')
+  const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=03a0a509d6ee48609eb80436232612&q=${location}`)
+  const info = await response.json()
+  console.log(info)
+  return info
+}
+
 function changeInfo(info) {
-  const cityTemp = info.current.temp_c
-  temp.textContent = `${cityTemp}&#176`
+  const tempTemp = info.current.temp_f
+  temp.textContent = `${tempTemp}` + String.fromCharCode(176)
   
   const conditionTemp = info.current.condition.text
   condition.textContent = `${conditionTemp}`
+ 
+  const cityTemp = info.location.name
+  city.textContent = `${cityTemp}`
 
-  
 }
 
-getWeather("Yorba Linda")
+function changeFuture(info) {
+  const highTemp = info.forecast.forecastday[0].day.maxtemp_f
+  const lowTemp = info.forecast.forecastday[0].day.mintemp_f
+
+  high.textContent = `H:${highTemp}` + String.fromCharCode(176)
+  low.textContent = `L:${lowTemp}` + String.fromCharCode(176)
+}
+
+
+search.addEventListener('submit',(e) => {
+  e.preventDefault()
+  const city = e.target[0].value
+  const current = getCurrent(city)
+  current.then(function(response){
+    changeInfo(response)
+  })
+  const future = getFuture(city) 
+  future.then(function(response){
+    changeFuture(response)
+  })
+  // changeInfo(current)
+})
+
